c587323953cb84bc712cfd5e1669060c6707372e
Wojciech Prusaczyk <woj.prusaczyk@gmail.com>
2024-06-27 00:50:21 +0200

    zmiana nazwy na poprawną

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../app-front/build')],
+        'DIRS': [os.path.join(BASE_DIR, '../frontend/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '../app-front/build/static'),  # Ścieżka do katalogu build
+    os.path.join(BASE_DIR, '../frontend/build/static'),  # Ścieżka do katalogu build
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
