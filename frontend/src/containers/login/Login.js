6ae7dc1ee06baeefa40d696bb37c294ad8418116
Wojciech Prusaczyk <woj.prusaczyk@gmail.com>
2024-07-01 20:50:44 +0200

    dodanie checkboxa, dostosowanei reszty formularza

--- frontend/src/containers/login/Login.js
+++ frontend/src/containers/login/Login.js
@@ -18,20 +18,20 @@ class Login extends Component {
                           <h2 className="login-form-title">Log into Eventful</h2>
                       </p>
                       <p>
-                          <a href="" className="login-form-company">
+                          <a href="" className="login-form-company" aria-label="login with google">
                               <img src={GoogleIcon} alt="google icon"/>
                               <span> Continue with Google </span>
                           </a>
                       </p>
                       <p>
-                          <a href="" className="login-form-company">
-                              <img src={FacebookIcon} alt="google icon"/>
+                          <a href="" className="login-form-company" aria-label="login with facebook">
+                              <img src={FacebookIcon} alt="facebook icon"/>
                               <span> Continue with Facebook </span>
                           </a>
                       </p>
                       <p>
-                          <a href="" className="login-form-company">
-                              <img src={AppleIcon} alt="google icon"/>
+                          <a href="" className="login-form-company" aria-label="login with apple">
+                              <img src={AppleIcon} alt="apple icon"/>
                               <span> Continue with Apple </span>
                           </a>
                       </p>
@@ -45,11 +45,12 @@ class Login extends Component {
                       <p>
                           <input type="password" title="Password" aria-label="Password" className="login-form-password" placeholder="Password"/>
                       </p>
-                      <p>
+                      <p className="login-form-remember">
                           <div className="login-form-remember-wrapper">
-                              <input className="login-form-remember tgl tgl-light" id="cb1-6" type="checkbox"/>
-                              <label title="Remember me" aria-label="Remember me" className="tgl-btn" htmlFor="cb1-6" />
+                              <input className="login-form-remember tgl tgl-light" id="cb1-6" type="checkbox" aria-label="Remember me"/>
+                              <label title="Remember me" aria-hidden="true" className="tgl-btn" htmlFor="cb1-6" />
                           </div>
+                          <span>Remember me</span>
                       </p>
                       <p>
                           <input type="submit" aria-label="Login" title="Login" value="Login"
@@ -62,14 +63,13 @@ class Login extends Component {
                       </p>
                   </form>
                   <p className="login-form-signUp">
-                      <span>No account?</span><a href="">Sign up</a>
+                      <span>No account?</span><a href={`${window.location.protocol}//${window.location.host}/register`}>Sign up</a>
                   </p>
               </main>
               <Footer/>
           </div>
       );
   }
-
 }
 
 export default Login;
