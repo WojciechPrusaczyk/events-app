9c4fcd89f80a88c3e735038a3c00eca697a55ba7
Adroksor <adrianlewandowski40@gmail.com>
2024-08-25 21:34:16 +0200

    template Email

--- frontend/src/containers/reset_password/ResetPassword.js
+++ frontend/src/containers/reset_password/ResetPassword.js
@@ -3,20 +3,47 @@ import React from 'react';
 import Header from "../../components/structure/header";
 import Footer from "../../components/structure/footer";
 import "../../styles/containers/home.scss"
+import axios from "axios";
+import {useParams} from "react-router-dom";
+
 
 const ResetPassword = () => {
+    const {token } =  useParams();
+    let new_password;
+    let new_password_repeat;
+
+    const resetPassword = (new_password) => {
+       axios
+          .post(`${window.location.protocol}//${window.location.host}/api/reset_password/`, {
+              password: new_password,
+              token: token
+          }).then(response => {
+          console.log(response)
+          })
+          .catch(error => {
+          console.log(error);
+          })
+
+   }
 
     return (
         <div>
             <Header />
             <main>
-                <h2>reset password</h2>
+                <h2>Nowe hasło</h2>
                 <ul>
-                    <li><a href={`${window.location.protocol}//${window.location.host}/register`}>Register</a></li>
-                    <li><a href={`${window.location.protocol}//${window.location.host}/login`}>Login</a></li>
+                    <input type="password" onChange={(e) => {
+                        new_password = e.target.value;
+                    }}/>
+                    <input type="password" onChange={(e) => {
+                        new_password_repeat = e.target.value;
+                    }}/>
+                    <input type="button" onClick={() => {
+                        resetPassword(new_password);
+                    }}/>
                 </ul>
             </main>
-            <Footer />
+            <Footer/>
         </div>
     );
 };
