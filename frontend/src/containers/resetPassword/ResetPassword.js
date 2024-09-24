9a1a5518a2e6f2d2cad8f936905ab09746d0eea5
Kacper <jambojeta12@wp.pl>
2024-09-24 17:15:02 +0200

    ResetPassword design and functionality

--- frontend/src/containers/resetPassword/ResetPassword.js
+++ frontend/src/containers/resetPassword/ResetPassword.js
@@ -1,49 +1,99 @@
-// src/containers/ResetPassword.js
-import React from 'react';
+import React, { useState } from 'react';
 import Header from "../../components/structure/header";
 import Footer from "../../components/structure/footer";
-import "../../styles/containers/home.scss"
+import "../../styles/containers/home.scss";
+import "../../styles/containers/newPassword.scss";
 import axios from "axios";
-import {useParams} from "react-router-dom";
-
+import { useParams } from "react-router-dom";
 
 const ResetPassword = () => {
-    const {token } =  useParams();
-    let new_password;
-    let new_password_repeat;
+    const { token } = useParams();
+    const [newPassword, setNewPassword] = useState("");
+    const [newPasswordRepeat, setNewPasswordRepeat] = useState("");
+    const [error, setError] = useState("");
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
-    const resetPassword = (new_password) => {
-       axios
-          .post(`${window.location.protocol}//${window.location.host}/api/reset-password/`, {
-              password: new_password,
-              token: token
-          }).then(response => {
-          console.log(response)
-          })
-          .catch(error => {
-          console.log(error);
-          })
+    const resetPassword = (event) => {
+        event.preventDefault();
+        setError("");
 
-   }
+        if (newPassword !== newPasswordRepeat) {
+            setError("Passwords do not match.");
+            return;
+        }
+
+        axios
+            .post(`${window.location.protocol}//${window.location.host}/api/reset-password/`, {
+                password: newPassword,
+                token: token
+            })
+            .then(response => {
+                if (response.status === 200) {
+                    setIsFormSubmitted(true);
+                    setError("");
+                } else {
+                    setError("Server side error, please try again later.");
+                }
+            })
+            .catch(() => {
+                setError("Error occurred, try again later.");
+            });
+    };
+
+    const NewPasswordForm = (
+        <form className="NewPasswordForm">
+            <div>
+                <h2>Enter your new password</h2>
+                <ul>
+                    <input
+                        type="password"
+                        onChange={(e) => setNewPassword(e.target.value)}
+                        className="login-form-email"
+                        placeholder="New password"
+                    />
+                    <input
+                        type="password"
+                        onChange={(e) => setNewPasswordRepeat(e.target.value)}
+                        className="login-form-email"
+                        placeholder="Repeat new password"
+                    />
+                    {error && <p><span className="resetError">{error}</span></p>}
+                    <input
+                        type="submit"
+                        onClick={resetPassword}
+                        className="form-container-confirmation-next btn-next"
+                        value="Reset Password"
+                    />
+                </ul>
+            </div>
+        </form>
+    );
+
+    const PasswordResetSuccess = (
+        <div className="confirmation">
+            <h2 className="form-page-title">Password Reset Successful</h2>
+            <h3 className="form-page-title">Your password has been successfully reset.</h3>
+            <input
+                onClick={() => {
+                    window.location.href = `${window.location.protocol}//${window.location.host}`;
+                }}
+                type="button"
+                aria-label="Go to the main page"
+                title="Go to the main page"
+                value="Go to the main page"
+                className="form-container-confirmation-next btn-next"
+            />
+        </div>
+    );
 
     return (
         <div>
             <Header />
             <main>
-                <h2>Nowe hasło</h2>
-                <ul>
-                    <input type="password" onChange={(e) => {
-                        new_password = e.target.value;
-                    }}/>
-                    <input type="password" onChange={(e) => {
-                        new_password_repeat = e.target.value;
-                    }}/>
-                    <input type="button" onClick={() => {
-                        resetPassword(new_password);
-                    }}/>
-                </ul>
+                {!isFormSubmitted && NewPasswordForm}
+                {isFormSubmitted && PasswordResetSuccess}
             </main>
-            <Footer/>
+            <Footer />
         </div>
     );
 };
