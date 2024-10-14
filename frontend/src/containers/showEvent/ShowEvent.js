09bb9783fdf76b9e6a8d5342ad3f0fab27a200ad
Wojciech Prusaczyk <woj.prusaczyk@gmail.com>
2024-10-14 17:19:50 +0200

    dokończenie wygladu viewEvent

--- frontend/src/containers/showEvent/ShowEvent.js
+++ frontend/src/containers/showEvent/ShowEvent.js
@@ -10,6 +10,8 @@ import { getAddressByLaLng } from "../../components/Helpers";
 import EventImage from "../../components/EventImage";
 import TimeIcon from "../../images/icons/clockIcon.svg"
 import DateIcon from "../../images/icons/dateIcon.svg"
+import CrownIcon from "../../images/icons/crownIcon.svg"
+import LocationPin from "../../images/icons/locationPinIcon.svg"
 
 const ShowEvent = () => {
     const { code } = useParams();
@@ -59,28 +61,32 @@ const ShowEvent = () => {
                             size={"large"}/>
             </p>
             <p>
-                {((eventData.location.placeId == null || eventData.location.placeId === "default") && eventData.location.formattedAddress == null) &&
-                    <a className={"show-event-address"}
+                {((eventData.location.placeId == null || eventData.location.placeId === "default") || eventData.location.formattedAddress == null || eventData.location.formattedAddress === "") &&
+                    <a className={"show-event-address"} target="_blank"
                        href={`https://maps.google.com/?q=${eventData.location.latitude},${eventData.location.longitude}`}>
+                        <img src={LocationPin} alt="location pin icon"/>
                         {address ? address : "Loading address."}
                     </a>
                 }
                 {(eventData.location.placeId !== null && eventData.location.placeId !== "default" && eventData.location.formattedAddress !== null) &&
-                    <a className={"show-event-address"}
+                    <a className={"show-event-address"} target="_blank"
                        href={`https://maps.google.com/?q=${eventData.location.latitude},${eventData.location.longitude}`}>
+                        <img src={LocationPin} alt="location pin icon"/>
                         {eventData.location.formattedAddress}
                     </a>
                 }
             </p>
             <p>
                 <div className={"show-event-date"}>
-                    <span>Start:</span>
-                    <span>{eventData.starttime.toLocaleDateString('en-US', {
+                    <span className={"show-event-date-title"}>Start:</span>
+                    <span className={"show-event-date-time"}><img src={DateIcon} alt="calendar icon"/>
+                        {eventData.starttime.toLocaleDateString('en-US', {
                         year: 'numeric',
                         month: 'long',
                         day: 'numeric'
                     })}</span>
-                    <span>{eventData.starttime.toLocaleTimeString('en-US', {
+                    <span className={"show-event-date-date"}><img src={TimeIcon} alt="time icon"/>
+                        {eventData.starttime.toLocaleTimeString('en-US', {
                         hour: '2-digit',
                         minute: '2-digit',
                         hour12: false
@@ -89,13 +95,15 @@ const ShowEvent = () => {
             </p>
             <p>
                 <div className={"show-event-date"}>
-                    <span>End:</span>
-                    <span>{eventData.endtime.toLocaleDateString('en-US', {
+                    <span className={"show-event-date-title"}>End:</span>
+                    <span className={"show-event-date-time"}><img src={DateIcon} alt="calendar icon" />
+                        {eventData.endtime.toLocaleDateString('en-US', {
                         year: 'numeric',
                         month: 'long',
                         day: 'numeric'
                     })}</span>
-                    <span>{eventData.endtime.toLocaleTimeString('en-US', {
+                    <span className={"show-event-date-date"}><img src={TimeIcon} alt="time icon"/>
+                        {eventData.endtime.toLocaleTimeString('en-US', {
                         hour: '2-digit',
                         minute: '2-digit',
                         hour12: false
@@ -103,7 +111,10 @@ const ShowEvent = () => {
                 </div>
             </p>
             <p>
-                <h1 className={"show-event-supervisor"}>{eventData.supervisor.username}</h1>
+                <h1 className={"show-event-supervisor"}>
+                    <img src={CrownIcon} alt="crown icon" />
+                    {eventData.supervisor.username}
+                </h1>
             </p>
             <p>
                 <button className={"show-event-join"}>{eventData.joinApproval ? "Join" : "Send join request"}</button>
