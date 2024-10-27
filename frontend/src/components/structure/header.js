1e207557f35121ffad5a20ed924e7ede3fb89209
WojciechPrusaczyk <woj.prusaczyk@gmail.com>
2024-10-27 23:19:33 +0100

    dokończenie powiadomień

--- frontend/src/components/structure/header.js
+++ frontend/src/components/structure/header.js
@@ -62,27 +62,50 @@ const CreateEvent = () => {
         });
 }
 
-const AcceptUser = async (userId, eventId, accepted) => {
-    axios
-        .post(`${window.location.protocol}//${window.location.host}/api/accept-user`, {
-                userId: userId,
-                eventId: eventId,
-                accepted: accepted,
-            }, { withCredentials: true })
-        .then(response => {
-            return response.status === 200;
-        })
-        .catch(err => {
-            console.log(err);
-            return false;
-        });
-}
+
+
+
 
 const Header = (props) => {
     const [isAuthenticated, setIsAuthenticated] = useState(null);
     const [user, setUser] = useState(null);
     const [notifications, setNotifications] = useState([]);
     const [notificationsShown, showNotifications] = useState(false);
+    const [isLoadingNotifications, setIsLoadingNotifications] = useState(true);
+
+    const GetNotifications = async () => {
+        axios
+            .post(`${window.location.protocol}//${window.location.host}/api/get-notifications`, {
+                username: Cookies.get('username')
+            },{ withCredentials: true })
+            .then(result => {
+                if (result.status === 200) {
+                    setNotifications(result.data.notifications);
+                    setIsLoadingNotifications(false);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                setIsLoadingNotifications(false);
+            });
+    }
+
+    const AcceptUser = async (userId, eventId, accepted) => {
+        axios
+            .post(`${window.location.protocol}//${window.location.host}/api/accept-user`, {
+                    userId: userId,
+                    eventId: eventId,
+                    accepted: accepted,
+                }, { withCredentials: true })
+            .then(response => {
+                GetNotifications();
+                return response.status === 200;
+            })
+            .catch(err => {
+                console.log(err);
+                return false;
+            });
+    }
 
     useEffect(() => {
         const checkAuth = async () => {
@@ -110,19 +133,7 @@ const Header = (props) => {
 
         if ( notifications.length === 0 )
         {
-            axios
-                .post(`${window.location.protocol}//${window.location.host}/api/get-notifications`, {
-                    username: Cookies.get('username')
-                },{ withCredentials: true })
-                .then(result => {
-                    if (result.status === 200) {
-                        setNotifications(result.data.notifications);
-                        console.log(result.data)
-                    }
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+            GetNotifications();
         }
 
     }, []);
@@ -132,14 +143,27 @@ const Header = (props) => {
 
     let notificationsList =  notifications.map((entity) => {
         return <li className={"notificationsList-item"}>
-            <span className={"notificationsList-item-text"}>{`User ${entity.user_name} is asking for acceptation.`}</span>
+            <span className={"notificationsList-item-text"}
+                  title={`User ${entity.user_name} is asking for acceptation to ${entity.event_name}.`} >
+                    User <b>{entity.user_name}</b> is asking for acceptation to <b>{entity.event_name}.</b>
+            </span>
             <button className={"notificationsList-item-btn accept"} onClick={() => {
+                setIsLoadingNotifications(true);
                 const isRequestSuccessfull = AcceptUser(entity.user_id, entity.event_id, true);
+
+                if (isRequestSuccessfull) {
+                    GetNotifications();
+                }
             }} title={"Accept"}>
                 <ConfirmIcon className="notificationsList-item-btn-icon accept" />
             </button>
             <button className={"notificationsList-item-btn refuse"} onClick={() => {
+                setIsLoadingNotifications(true);
                 const isRequestSuccessfull = AcceptUser(entity.user_id, entity.event_id, false);
+
+                if (isRequestSuccessfull) {
+                    GetNotifications();
+                }
             }} title={"Refuse"}>
                 <CloseIcon className="notificationsList-item-btn-icon refuse" />
             </button>
@@ -156,18 +180,26 @@ const Header = (props) => {
         <button aria-label="add new event" className="header-user-button" onClick={CreateEvent}>
             <img src={addIcon} alt="add new event" aria-hidden={true}/>
         </button>
-        <button aria-label="add new event" className="header-user-button">
-            <img src={notificationIcon} alt="show notifications" aria-hidden={true} onClick={ () => {showNotifications(!notificationsShown)}}/>
+        <button aria-label="add new event" className={`header-user-button${(notificationsList.length > 0)?" ping":""}`} onClick={ () => {
+                setIsLoadingNotifications(true);
+                GetNotifications();
+                showNotifications(!notificationsShown);
+            }}>
+            {(notificationsList.length > 0) && <div className={"ping-mark"}></div>}
+            <img src={notificationIcon} alt="show notifications" aria-hidden={true}/>
         </button>
         { notificationsShown &&
             <div id={"notifications"}>
-                {(notificationsList.length > 0) &&
+                {((notificationsList.length > 0) && !isLoadingNotifications) &&
                     <ul className={"notificationsList"}>
                         {notificationsList}
                     </ul>
                 }
-                {!(notificationsList.length > 0) &&
-                    <div className={"notificationsList"}>No notifications found.</div>
+                {(!(notificationsList.length > 0) && !isLoadingNotifications) &&
+                    <div className={"notificationsDiv"}>No notifications found.</div>
+                }
+                {isLoadingNotifications &&
+                    <div className={"notificationsDiv"}>Loading...</div>
                 }
             </div>
         }
